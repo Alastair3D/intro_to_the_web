9b53e278afaa8282f3e1799e530b5e8a34ea7afd
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-30 15:18:59 +0100

    red dash border around cat image

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra'
 require 'shotgun'
 
+set :session_secret, 'super secret'
+
 get '/' do
-  "Hello!"
+  "Hey there!"
 end
 
 get '/secret' do
-  "Tell me your secrets"
+  "Tell me your secrets, please"
+end
+
+get '/cat' do
+# border: dashed red; http://bit.ly/1eze8aE
+  "<div style='border: 3px dashed red'> <img src='http://bit.ly/1eze8aE'> </div>"
 end
